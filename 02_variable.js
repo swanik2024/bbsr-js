5968fbbc47bff5115b58f1aa9265069a557cbf54
nikhil-fd <nikhildas.fd@gmail.com>
2024-06-09 09:27:08 +0530

    variable explained

--- 02_variable.js
+++ 02_variable.js
@@ -1,2 +1,17 @@
+//variable re amey to jemiti hou value assign karidauchey but taku final analyse/check
+//karuchi "NodeJS" whether the written code is correct or not if correct then "NodeJS"
+//execute the code orelese return error.
+//variable is a location in memory where we can store a value in string, number, boolean format.
+//variable is defined in 3 keyword var, let, const.
 let myName = "Nikhil Das";
+myName = "swati"
 console.log(myName);
+
+var mySkill = "SAP";
+mySkill = "React JS"
+console.log(mySkill);
+
+const myExp = 3;
+// myExp = 5;
+console.log(myExp);
+console.table([myName,mySkill,myExp]);
